5d919b85254613539274330dcac6cfdeabdbfc03
Plazf01 <plazf01@mail.sunysuffolk.edu>
2017-05-16 01:35:23 -0400

    Add files via upload

---
+++ Fernando html and model/Business.java
@@ -0,0 +1,49 @@
+public class Business {
+
+	private String businessName;
+	private String location;
+	private String zipCode;
+	private Event[] events;
+	private String serialNumber;
+
+	public String getBusinessName() {
+		return businessName;
+	}
+
+	public void setBusinessName(String businessName) {
+		this.businessName = businessName;
+	}
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+
+	public String getZipCode() {
+		return zipCode;
+	}
+
+	public void setZipCode(String zipCode) {
+		this.zipCode = zipCode;
+	}
+
+	public Event[] getEvents() {
+		return events;
+	}
+
+	public void setEvents(Event[] events) {
+		this.events = events;
+	}
+
+	public String getSerialNumber() {
+		return serialNumber;
+	}
+
+	public void setSerialNumber(String serialNumber) {
+		this.serialNumber = serialNumber;
+	}
+
+}

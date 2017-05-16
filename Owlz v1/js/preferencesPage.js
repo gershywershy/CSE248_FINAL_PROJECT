1f08a120f3a6e7b9c3426204c40eb807f11ba044
gershywershy <raymg32@mail.sunysuffolk.edu>
2017-05-16 08:55:35 -0400

    Add files via upload
    Testing put javascript and tried to implement it in HTML

---
+++ Owlz v1/js/preferencesPage.js
@@ -0,0 +1,14 @@
+function musicChoice(){
+	music = document.getElementById("musicChoice").value;
+	alert(music);
+}
+
+function venueChoice(){
+	 venue = document.getElementById("venueChoice").value;
+	alert(venue);
+}
+
+function genderChoice(){
+	gender = document.getElementById("genderChoice").value;
+	alert(gender);
+}
